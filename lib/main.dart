287e51c2403afce11568793e9142e676dfbd6765
tylersaitang <tylersaitang@gmail.com>
2022-04-07 10:31:10 -0500

    added search bar to map screen to search for buldings by name or code. (not yet fully implemented to go to marker location).

--- lib/main.dart
+++ lib/main.dart
@@ -266,8 +266,87 @@ class MapState extends State<MapWindow> {
         drawer: NavDrawer(),
         appBar: AppBar(
           title: Text('Campus Map'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: (){
+                showSearch(
+                  context: context,
+                  delegate: MySearchDelegate(),
+                );
+              },
+            )
+          ]
         ));
   }
+}
+
+//Search Bar for Buildings
+class MySearchDelegate extends SearchDelegate {
+  List<String> searchResults = [
+    'J.B. Hunt Transport Services Inc. Center for Academic Excellence (JBHT)',
+    'Champions Hall (CHPM)',
+    'Willard J. Walker Hall (WJWH)',
+    'Mechanical Engineering (MECH)',
+    'Physics Building (PHYS)',
+  ];
+
+  @override
+  Widget? buildLeading(BuildContext context) =>
+      IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () => close(context, null), //close searchbar
+      );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) =>
+      [
+        IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null); //close searchbar
+            } else {
+              query = '';
+            }
+          },
+        ),
+      ];
+
+  @override
+  Widget buildResults(BuildContext context) => Center(
+        child: Text(
+          query,
+          style: const TextStyle(fontSize: 64, fontWeight: FontWeight.bold)
+        )
+      );
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = searchResults.where((searchResult) {
+      final result = searchResult.toLowerCase();
+      final input = query.toLowerCase();
+
+      return result.contains(input);
+    }).toList();
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        final suggestion = suggestions[index];
+
+        return ListTile(
+          title: Text(suggestion),
+          onTap: () {
+            query = suggestion;
+
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+
 }
 
 // Function to build the popup layouts for each building
